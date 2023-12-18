e245a5ae02fcab9d935ad6cf9f344c58588d694f
Xiaoqi Zhao <xiaoqi.zhao@volvo.com>
2023-12-17 21:44:55 -0500

    working in ch01

--- RinAction/RinAction.mm
+++ RinAction/RinAction.mm
@@ -1,9 +1,9 @@
 <map version="freeplane 1.11.5">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="R in Action&#xa;R语言实战" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1702838559762">
+<node TEXT="R in Action&#xa;R语言实战" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1702860420180" LINK="https://zh.wikipedia.org/wiki/%E6%89%8E%E9%81%94%E7%88%BE">
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<font BOLD="true"/>
-<hook NAME="MapStyle" background="#f9f9f8">
+<font NAME="STKaiti" SIZE="15" BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.3">
     <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_nord_template.mm" fit_to_viewport="false"/>
 
 <map_styles>
@@ -79,9 +79,36 @@
 </stylenode>
 </map_styles>
 </hook>
-<node TEXT="第一部分 入门" POSITION="bottom_or_right" ID="ID_1040999408" CREATED="1702838576207" MODIFIED="1702838581562">
-<node TEXT="第1章 R语言介绍" ID="ID_581100029" CREATED="1702838583604" MODIFIED="1702838594228">
-<node TEXT="1.1 为何要使用R" ID="ID_1460659211" CREATED="1702840079882" MODIFIED="1702840093334">
+<richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      本书的封面图片标题是“来自扎达尔的男人”。这张图片取自19世纪中期Nikola Arsenovic的一本克罗地亚传统服饰图集的复刻版，由克罗地亚斯普利特的Ethnographic博物馆在2003年出版。图片由Ethnographic博物馆一位热心的图书管理员提供。斯普利特在中世纪时是罗马帝国的核心。从大概公元304年起，卸任的帝国国王戴克里安（Diocletian）所居住的皇宫就在这里。这本书中涵盖了克罗地亚各个地区色彩斑斓的图片，并对服饰和日常生活进行了介绍。
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      本书的封面图片标题是“来自扎达尔的男人”。这张图片取自19世纪中期Nikola Arsenovic的一本克罗地亚传统服饰图集的复刻版，由克罗地亚斯普利特的Ethnographic博物馆在2003年出版。图片由Ethnographic博物馆一位热心的图书管理员提供。斯普利特在中世纪时是罗马帝国的核心。从大概公元304年起，卸任的帝国国王戴克里安（Diocletian）所居住的皇宫就在这里。这本书中涵盖了克罗地亚各个地区色彩斑斓的图片，并对服饰和日常生活进行了介绍。
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      https://zh.wikipedia.org/wiki/%E6%89%8E%E9%81%94%E7%88%BE
+    </p>
+  </body>
+</html>
+</richcontent>
+<node TEXT="第一部分 入门" POSITION="bottom_or_right" ID="ID_1040999408" CREATED="1702838576207" MODIFIED="1702859385035">
+<font NAME="STKaiti"/>
+<node TEXT="第1章 R语言介绍" ID="ID_581100029" CREATED="1702838583604" MODIFIED="1702859385038">
+<font NAME="STKaiti"/>
+<node TEXT="1.1 为何要使用R" FOLDED="true" ID="ID_1460659211" CREATED="1702840079882" MODIFIED="1702859385039">
+<font NAME="STKaiti"/>
 <node TEXT="R是免费的！" ID="ID_1680253253" CREATED="1702840101487" MODIFIED="1702840187890">
 <icon BUILTIN="ksmiletris"/>
 </node>
@@ -107,40 +134,152 @@
 <icon BUILTIN="smiley-neutral"/>
 </node>
 </node>
-<node TEXT="1.1 R的安装" ID="ID_523596895" CREATED="1702839901724" MODIFIED="1702839910351"/>
-<node TEXT="1.2 熟悉R语言" ID="ID_1240422880" CREATED="1702839913198" MODIFIED="1702839920212"/>
-<node TEXT="1.3 运行R程序" ID="ID_1080260170" CREATED="1702839920604" MODIFIED="1702839927371"/>
+<node TEXT="1.2 R的安装" FOLDED="true" ID="ID_523596895" CREATED="1702839901724" MODIFIED="1702859385018">
+<font NAME="STKaiti"/>
+<node TEXT="CRAN http://cran.r-project.org" ID="ID_487572270" CREATED="1702849371871" MODIFIED="1702859385041" LINK="http://cran.r-project.org">
+<font NAME="STKaiti"/>
 </node>
-<node TEXT="第2章 创建数据集" ID="ID_955565794" CREATED="1702838598190" MODIFIED="1702838607535"/>
-<node TEXT="第3章 图形初阶" ID="ID_1846090514" CREATED="1702838610957" MODIFIED="1702838623963"/>
-<node TEXT="第4章 基本数据管理" ID="ID_1298743545" CREATED="1702838624902" MODIFIED="1702838638207"/>
-<node TEXT="第5章 高级数据管理" ID="ID_4966214" CREATED="1702838641732" MODIFIED="1702838649370"/>
+<node TEXT="RStudio" ID="ID_1283182456" CREATED="1702849416567" MODIFIED="1702859385044" LINK="https://posit.co/download/rstudio-desktop/">
+<font NAME="STKaiti"/>
 </node>
-<node TEXT="第二部分 基本方法" POSITION="bottom_or_right" ID="ID_506229477" CREATED="1702838652558" MODIFIED="1702838658254">
-<node TEXT="第6章 基本图形" ID="ID_785274694" CREATED="1702838658259" MODIFIED="1702838666702"/>
-<node TEXT="第7章 基本统计分析" ID="ID_998351133" CREATED="1702838666920" MODIFIED="1702838678368"/>
 </node>
-<node TEXT="第三部分 中级方法" POSITION="bottom_or_right" ID="ID_315348194" CREATED="1702838682988" MODIFIED="1702838691422">
-<node TEXT="第8章 回归" ID="ID_1794491753" CREATED="1702838691428" MODIFIED="1702838697808"/>
-<node TEXT="第9章 方差分析" ID="ID_1616525394" CREATED="1702838698253" MODIFIED="1702838733873"/>
-<node TEXT="第10章 功效分析" ID="ID_1347748678" CREATED="1702838736702" MODIFIED="1702838744122"/>
-<node TEXT="第11章 中级绘图" ID="ID_1339237322" CREATED="1702838746205" MODIFIED="1702838754673"/>
-<node TEXT="第12章 重抽样与自助法" ID="ID_1835112720" CREATED="1702838761981" MODIFIED="1702838774632"/>
+<node TEXT="1.3 熟悉R语言" ID="ID_1240422880" CREATED="1702839913198" MODIFIED="1702859385044">
+<font NAME="STKaiti"/>
+<node TEXT="1.3.1 新手上路" POSITION="bottom_or_right" ID="ID_1080260170" CREATED="1702839920604" MODIFIED="1702860459244">
+<font NAME="STKaiti"/>
 </node>
-<node TEXT="第四部分 高级方法" POSITION="bottom_or_right" ID="ID_1156984275" CREATED="1702838789919" MODIFIED="1702838798158">
-<node TEXT="第13章 广义线性模型" POSITION="bottom_or_right" ID="ID_1187151001" CREATED="1702838775015" MODIFIED="1702838789054"/>
-<node TEXT="第14章 主成分分析和因子分析" POSITION="bottom_or_right" ID="ID_282358969" CREATED="1702838805474" MODIFIED="1702838822665"/>
-<node TEXT="第15章 时间序列" POSITION="bottom_or_right" ID="ID_1098899157" CREATED="1702838823007" MODIFIED="1702838829704"/>
-<node TEXT="第16章 聚类分析" POSITION="bottom_or_right" ID="ID_1339894872" CREATED="1702838829983" MODIFIED="1702838838410"/>
-<node TEXT="第17章 分类" POSITION="bottom_or_right" ID="ID_1780709411" CREATED="1702838841463" MODIFIED="1702838847944"/>
-<node TEXT="第18章 处理缺失数据的高级方法" POSITION="bottom_or_right" ID="ID_1405044864" CREATED="1702838848373" MODIFIED="1702838861405"/>
+<node TEXT="1.3.2 获取帮助" POSITION="bottom_or_right" ID="ID_118146924" CREATED="1702849539855" MODIFIED="1702860459257">
+<font NAME="STKaiti"/>
 </node>
-<node TEXT="第五部分 技能拓展" POSITION="bottom_or_right" ID="ID_292870234" CREATED="1702838864951" MODIFIED="1702838873857">
-<node TEXT="第19章 使用ggplot2进行高级绘图" ID="ID_1313348239" CREATED="1702838873861" MODIFIED="1702838886326"/>
-<node TEXT="第20章 高级编程" ID="ID_1275259443" CREATED="1702838890865" MODIFIED="1702838896569"/>
-<node TEXT="第21章 创建包" ID="ID_467461126" CREATED="1702838896927" MODIFIED="1702838906959"/>
-<node TEXT="第22章 创建动态报告" ID="ID_1537107294" CREATED="1702838907839" MODIFIED="1702838915658"/>
-<node TEXT="第23章 使用lattice进行高级绘图" ID="ID_469191306" CREATED="1702838915815" MODIFIED="1702838930187"/>
+<node TEXT="1.3.3 工作空间" POSITION="bottom_or_right" ID="ID_888114439" CREATED="1702849559494" MODIFIED="1702860459258">
+<font NAME="STKaiti"/>
+<node TEXT="getwd(): 显示当前工作目录" ID="ID_1337868177" CREATED="1702849580504" MODIFIED="1702860484795">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="setwd(&quot;mydir&quot;): 修改当前工作目录为mydir" ID="ID_219711085" CREATED="1702849598472" MODIFIED="1702860484808">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="ls(): 列出当前工作空间中的对象" ID="ID_344093896" CREATED="1702849796502" MODIFIED="1702860484809">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="rm(objectlist): 移除（删除）一个或多个对象" ID="ID_1810227113" CREATED="1702849817278" MODIFIED="1702860484811">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="rm(list = ls()): 移除（删除）全部对象" ID="ID_1840786026" CREATED="1702849840358" MODIFIED="1702860484814">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="help(options): 显示可用选项的说明" ID="ID_62509584" CREATED="1702850094401" MODIFIED="1702860484815">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="options(): 显示或设置当前选项" ID="ID_701749288" CREATED="1702850116375" MODIFIED="1702860484816">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="history(#): 显示最近使用过的#个命令（默认为25）" ID="ID_19978067" CREATED="1702850129892" MODIFIED="1702860484817">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="cat(&quot;\014&quot;): 打开httpd帮助服务器" ID="ID_1445772291" CREATED="1702850040619" MODIFIED="1702860484819">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="Ctrl+L: clear console" ID="ID_107617248" CREATED="1702849939373" MODIFIED="1702860484821">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="q(): 退出R" ID="ID_470493050" CREATED="1702849617814" MODIFIED="1702860484821">
+<font NAME="STKaiti"/>
+</node>
+</node>
+<node TEXT="1.3.4 输入和输出" POSITION="bottom_or_right" ID="ID_1558122083" CREATED="1702850248212" MODIFIED="1702860459259">
+<font NAME="STKaiti"/>
+<node TEXT="1. 输入" ID="ID_1238349414" CREATED="1702865984402" MODIFIED="1702865994303">
+<node TEXT="source(&quot;filename&quot;)" ID="ID_29149048" CREATED="1702865997746" MODIFIED="1702866004590"/>
+</node>
+<node TEXT="2. 文本输出" ID="ID_1860987841" CREATED="1702865994908" MODIFIED="1702866010965">
+<node TEXT="sink(&quot;filename&quot;)" ID="ID_1064856722" CREATED="1702866010967" MODIFIED="1702866018540"/>
+</node>
+</node>
+</node>
+<node TEXT="1.4 R的包" FOLDED="true" ID="ID_1953283157" CREATED="1702849495494" MODIFIED="1702859385046">
+<font NAME="STKaiti"/>
+<node TEXT="1.4.1 包的概念" ID="ID_903381813" CREATED="1702849504018" MODIFIED="1702849515104"/>
+</node>
+</node>
+<node TEXT="第2章 创建数据集" ID="ID_955565794" CREATED="1702838598190" MODIFIED="1702859385046">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第3章 图形初阶" ID="ID_1846090514" CREATED="1702838610957" MODIFIED="1702859385048">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第4章 基本数据管理" ID="ID_1298743545" CREATED="1702838624902" MODIFIED="1702859385049">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第5章 高级数据管理" ID="ID_4966214" CREATED="1702838641732" MODIFIED="1702859385051">
+<font NAME="STKaiti"/>
+</node>
+</node>
+<node TEXT="第二部分 基本方法" POSITION="bottom_or_right" ID="ID_506229477" CREATED="1702838652558" MODIFIED="1702859385052">
+<font NAME="STKaiti"/>
+<node TEXT="第6章 基本图形" ID="ID_785274694" CREATED="1702838658259" MODIFIED="1702859385052">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第7章 基本统计分析" ID="ID_998351133" CREATED="1702838666920" MODIFIED="1702859385053">
+<font NAME="STKaiti"/>
+</node>
+</node>
+<node TEXT="第三部分 中级方法" POSITION="bottom_or_right" ID="ID_315348194" CREATED="1702838682988" MODIFIED="1702859385055">
+<font NAME="STKaiti"/>
+<node TEXT="第8章 回归" ID="ID_1794491753" CREATED="1702838691428" MODIFIED="1702859385056">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第9章 方差分析" ID="ID_1616525394" CREATED="1702838698253" MODIFIED="1702859385057">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第10章 功效分析" ID="ID_1347748678" CREATED="1702838736702" MODIFIED="1702859385058">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第11章 中级绘图" ID="ID_1339237322" CREATED="1702838746205" MODIFIED="1702859385058">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第12章 重抽样与自助法" ID="ID_1835112720" CREATED="1702838761981" MODIFIED="1702859385060">
+<font NAME="STKaiti"/>
+</node>
+</node>
+<node TEXT="第四部分 高级方法" POSITION="bottom_or_right" ID="ID_1156984275" CREATED="1702838789919" MODIFIED="1702859385061">
+<font NAME="STKaiti"/>
+<node TEXT="第13章 广义线性模型" POSITION="bottom_or_right" ID="ID_1187151001" CREATED="1702838775015" MODIFIED="1702859385061">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第14章 主成分分析和因子分析" POSITION="bottom_or_right" ID="ID_282358969" CREATED="1702838805474" MODIFIED="1702859385062">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第15章 时间序列" POSITION="bottom_or_right" ID="ID_1098899157" CREATED="1702838823007" MODIFIED="1702859385064">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第16章 聚类分析" POSITION="bottom_or_right" ID="ID_1339894872" CREATED="1702838829983" MODIFIED="1702859385065">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第17章 分类" POSITION="bottom_or_right" ID="ID_1780709411" CREATED="1702838841463" MODIFIED="1702859385066">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第18章 处理缺失数据的高级方法" POSITION="bottom_or_right" ID="ID_1405044864" CREATED="1702838848373" MODIFIED="1702859385066">
+<font NAME="STKaiti"/>
+</node>
+</node>
+<node TEXT="第五部分 技能拓展" POSITION="bottom_or_right" ID="ID_292870234" CREATED="1702838864951" MODIFIED="1702859385067">
+<font NAME="STKaiti"/>
+<node TEXT="第19章 使用ggplot2进行高级绘图" ID="ID_1313348239" CREATED="1702838873861" MODIFIED="1702859385068">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第20章 高级编程" ID="ID_1275259443" CREATED="1702838890865" MODIFIED="1702859385070">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第21章 创建包" ID="ID_467461126" CREATED="1702838896927" MODIFIED="1702859385070">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第22章 创建动态报告" ID="ID_1537107294" CREATED="1702838907839" MODIFIED="1702859385070">
+<font NAME="STKaiti"/>
+</node>
+<node TEXT="第23章 使用lattice进行高级绘图" ID="ID_469191306" CREATED="1702838915815" MODIFIED="1702859385071">
+<font NAME="STKaiti"/>
+</node>
 </node>
 </node>
 </map>
